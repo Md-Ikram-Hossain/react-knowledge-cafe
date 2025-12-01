43eba7240834f015c4c385f0b4e18cf039e67db9
ikramiiss <ikramhossian22@gmail.com>
2025-12-01 17:44:26 +0600

    Bookmark section added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,36 @@
+import { IoBookmarksSharp } from "react-icons/io5";
 
+const Blog = ({ blog, handleAddToBookmark }) => {
+    const { title, cover, author, author_img, reading_time, posted_data,hashtags } = blog;
 
-const Blog = ({ blog }) => {
-    console.log(blog)
     return (
-        <div>
+        <div className="mb-20">
+            <img className="w-full mb-8" src={cover} alt={`Cover picture of the title ${title}`} />
             
+            <div className="flex justify-between mb-4">
+                <div className="flex">
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className="ml-6">
+                        <h3 className="text-2xl">{author}</h3>
+                        <p>{posted_data}</p>
+                    </div>
+
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmark(blog)} className="ml-2 text-2xl text-red-600"> <IoBookmarksSharp></IoBookmarksSharp> </button>
+
+                </div>
+            </div>
+
+
+
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,18 @@
+import Bookmark from "../Boookmark/Bookmark";
 
 
-const Bookmarks = () => {
+
+const Bookmarks = ({bookmarks}) => {
     return (
-        <div className="md:w-1/3">
-            <h1>Bookmarks</h1>
+        <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
+            <h1 className="text-3xl text-center">Bookmarked Blogs: {bookmarks.length}</h1>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
 
 export default Bookmarks;
+
+
